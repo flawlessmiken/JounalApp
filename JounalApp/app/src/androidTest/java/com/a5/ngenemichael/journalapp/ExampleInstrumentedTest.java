9f9b50418a6e451b5fd8894738b72aefe71cd2a7
flawlessmiken <honmichael.5@gmail.com>
2018-06-30 10:32:25 -1200

    last  commit

--- JounalApp/app/src/androidTest/java/com/a5/ngenemichael/journalapp/ExampleInstrumentedTest.java
+++ JounalApp/app/src/androidTest/java/com/a5/ngenemichael/journalapp/ExampleInstrumentedTest.java
@@ -1,13 +1,30 @@
 package com.a5.ngenemichael.journalapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.a5.ngenemichael.journalapp.Activities.AddJournalActivity;
+import com.a5.ngenemichael.journalapp.Activities.MainActivity;
+import com.a5.ngenemichael.journalapp.Activities.SingleDisplayActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.Matchers.allOf;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -23,4 +40,23 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.a5.ngenemichael.journalapp", appContext.getPackageName());
     }
+
+
+    @RunWith(AndroidJUnit4.class)
+    public class TestMainActivity {
+        @Rule
+        public ActivityTestRule<AddJournalActivity> activityActivityTestRule = new ActivityTestRule<AddJournalActivity>(AddJournalActivity.class);
+
+    }
+
+    @Test
+    public void TestFab(){
+         ActivityTestRule<AddJournalActivity> activityActivityTestRule = new ActivityTestRule<AddJournalActivity>(AddJournalActivity.class);
+
+        onView(withId(R.id.gridview)).perform(click());
+        onView(withId(R.id.text_mood)).perform(clearText(),typeText("Jou"));
+    }
+
+
+
 }
